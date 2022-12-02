89673b6a4c780ba8b1827ef2dd5de67085bb6a1d
camdenpowell <cpowell9218@sdsu.edu>
2022-12-01 19:02:01 -0800

    Start Scene 1.0
    First design of Start Scene
    -Missing Entire Interface :(

---
+++ Assets/ObstacleCoursePack/Scripts/BallSpawner.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public float timerSet = 7;
+    private float timeRemaining = 1;
+    //public float maxBalls = 10;
+    //private float totalBalls = 0;
+
+// Update is called once per frame
+    void Spawn()
+    {
+        GameObject cloneBall = (GameObject)Instantiate(ballPrefab, transform.position, transform.rotation);
+        //totalBalls++;
+        timeRemaining = timerSet;
+       // if (totalBalls >= maxBalls) { Destroy(cloneBall); }
+    }
+    void Update()
+    {
+    timeRemaining -= Time.deltaTime;
+    if(timeRemaining <= 0)
+        {
+            Spawn();
+        }
+    }
+}
