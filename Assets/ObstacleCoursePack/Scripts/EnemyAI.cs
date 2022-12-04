7c3c9f62c965117cc7c180d723a8552d87f2dff3
ch72 <chong2744sdsu@gmail.com>
2022-12-03 16:25:35 -0800

    Add enemy AI that follows player

---
+++ Assets/ObstacleCoursePack/Scripts/EnemyAI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (playerInSightRange) ChasePlayer();
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+        transform.LookAt(player);
+    }
+}
