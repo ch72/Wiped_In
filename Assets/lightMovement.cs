65873edd2ec76ad1898e7e82015fbe598927ba73
lindsayly <lindsay.ly1225@gmail.com>
2022-12-07 13:47:43 -0800

    updated course and added music

--- Assets/lightMovement.cs
+++ Assets/lightMovement.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class lightMovement : MonoBehaviour
 {
     private Vector3 dir = Vector3.left;
+    private Vector3 startPos;
+    public float offset = 0f; //If yo want to modify the position at the start
+    public float speed = 5f;
+
     // Start is called before the first frame update
-    void Start(){}
+    void Start(){
+          startPos = transform.position;
+          transform.position += Vector3.right * offset;
+    }
 
     // Update is called once per frame
     void Update(){
-        transform.Translate(dir*5*Time.deltaTime);
+          transform.Translate(dir*speed*Time.deltaTime);
  
-      if(transform.position.x <= -4){
-           dir = Vector3.right;
-      }
-      else if(transform.position.x >= 4){
-           dir = Vector3.left;
-      }
+          if(transform.position.x <= startPos.x - 5){
+               dir = Vector3.right;
+          }
+          else if(transform.position.x >= startPos.x + 5){
+               dir = Vector3.left;
+          }
     }
 }
