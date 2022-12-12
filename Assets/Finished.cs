0b8d677a876843d3a1ba9c2ca0fad1ba0d145ce5
jdenver23 <denverjacob23@gmail.com>
2022-12-11 23:42:55 -0800

    Finish Line Detection
    Fixed issue where finish line wouldn't detect player

--- Assets/Finished.cs
+++ Assets/Finished.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Finished : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -10,11 +10,12 @@ public class NewBehaviourScript : MonoBehaviour
         
     }
 
-    void OnTriggerEnter(Collider col){
-        if(col.gameObject.tag == "Player"){
-            Debug.Log("here");
+    void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
             Stopwatch.StopStopwatch();
-        }
-    }
+		}
+	}
 }
 
