474f1529d4d10ba015ff658ea80b6952b92d80ab
camdenpowell <cpowell9218@sdsu.edu>
2022-12-12 18:53:59 -0800

    Fixed pulsing.cs
    shoutout chatGPT

--- Assets/Pulsing.cs
+++ Assets/Pulsing.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class Pulsing : MonoBehaviour
 {
-    [SerializeField] private AnimationCurve myCurve;
+    float targetSize;
+
+    public float smoothSpeed = 0.25f;
+    public float maxScale = 1.5f;
+    public float minScale = 0.5f;
+
+    private float targetScale;
+
+    void Start()
+    {
+        targetScale = minScale;
+    }
 
     void Update()
-        transform.position = new Vector3(transform.position.x, myCurve.Evaluate((Time.time % myCurve.length)), transform.position.z);
+    {
+        transform.localScale = UnityEngine.Vector3.Lerp(transform.localScale, targetScale * UnityEngine.Vector3.one, smoothSpeed);
+        if(Mathf.Abs(transform.localScale.x - targetScale) < 0.01f)
+        {
+            targetScale = (targetScale == minScale) ? maxScale : minScale;
+        }
     }
 }
